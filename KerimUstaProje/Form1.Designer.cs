0f8b9060adf0e11119c5b634e837878fa948fba3
Kerim Usta <142199478+KerimUsta@users.noreply.github.com>
2023-09-28 08:30:41 +0300

    Add project files.

---
+++ KerimUstaProje/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace KerimUstaProje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.onaylaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.onaysızToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1206, 230);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(523, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(683, 230);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::KerimUstaProje.Properties.Resources.icons8_excel_96;
+            this.pictureBox1.Location = new System.Drawing.Point(534, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(97, 84);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(420, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(229, 65);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "LİSTELE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(16, 22);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label4.Size = new System.Drawing.Size(119, 39);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Durum";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 29;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Tumu"});
+            this.comboBox1.Location = new System.Drawing.Point(151, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(229, 37);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(523, 230);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(11, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Müşteri Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sipariş No";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(278, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 36);
+            this.textBox1.TabIndex = 4;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(278, 194);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 30);
+            this.dateTimePicker2.TabIndex = 7;
+            this.dateTimePicker2.Value = new System.DateTime(2018, 2, 1, 0, 0, 0, 0);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(278, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(206, 36);
+            this.textBox2.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(278, 145);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(206, 30);
+            this.dateTimePicker1.TabIndex = 6;
+            this.dateTimePicker1.Value = new System.DateTime(2018, 2, 1, 0, 0, 0, 0);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(11, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 39);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Teslim Tarihi";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 230);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1206, 490);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellMouseUp += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseUp);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.onaylaToolStripMenuItem,
+            this.onaysızToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(130, 52);
+            this.contextMenuStrip1.Click += new System.EventHandler(this.contextMenuStrip1_Click);
+            // 
+            // onaylaToolStripMenuItem
+            // 
+            this.onaylaToolStripMenuItem.Name = "onaylaToolStripMenuItem";
+            this.onaylaToolStripMenuItem.Size = new System.Drawing.Size(129, 24);
+            this.onaylaToolStripMenuItem.Text = "Onayla";
+            // 
+            // onaysızToolStripMenuItem
+            // 
+            this.onaysızToolStripMenuItem.Name = "onaysızToolStripMenuItem";
+            this.onaysızToolStripMenuItem.Size = new System.Drawing.Size(129, 24);
+            this.onaysızToolStripMenuItem.Text = "Onaysız";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1206, 720);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test Proje";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem onaylaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem onaysızToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ KerimUstaProje/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+
+namespace KerimUstaProje
+{
+    public partial class Form1 : Form
+    {
+        private int rowIndex = 0;
+
+        private static string ConnectionString =
+           "Provider=MSDAORA.1;Data Source=192.168.0.150/TEST;Persist Security Info=True;Password=ifsapp;User ID=ifsapp";
+        public Form1()
+        {
+           
+            InitializeComponent();
+            ComboDoldur();
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var durum = "";
+            if (comboBox1.SelectedIndex == 0)
+            {
+                durum = "%";
+            }
+            else
+            {
+                durum = comboBox1.SelectedItem.ToString();
+            }
+            using (OleDbConnection connection = new OleDbConnection(ConnectionString))
+            {
+                connection.Open();
+                OleDbCommand cmd = new OleDbCommand("select  k.Siparis_No,    k.Yayin,    k.Redüktör_Kod,    k.Redüktör_Aciklama,   k.Adet,   k.Fiyat,    k.Toplam_Siparis_Tutar,    k.Teslim_Tarihi,   k.Durum,  k.Gecikme  ,k.onay,    k.Müsteri_Ad from YR_KERIMUSTA k where teslim_tarihi >= '" + dateTimePicker1.Value.ToShortDateString() + "' and teslim_tarihi <= '" + dateTimePicker2.Value.ToShortDateString() + "' and k.Müsteri_Ad like '" +  textBox1.Text.ToUpper() + "%" + "'and k.siparis_no like '" +  textBox2.Text.ToUpper() + "%" + "' and k.durum like '" +  durum + "%" + "'ORDER BY YAYIN", connection);
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                // DataGridView kontrolüne verileri yükle
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+        public void ComboDoldur()
+        {
+            // Oracle veritabanına bağlan
+            using (OleDbConnection connection = new OleDbConnection(ConnectionString))
+            {
+                connection.Open();
+
+                // DISTINCT kullanarak benzersiz "STATE" değerlerini al
+                OleDbCommand cmd = new OleDbCommand("select DISTINCT TRIM(LEADING ' ' FROM X.objstate) as DURUM from CUSTOMER_ORDER_LINE x ORDER BY 1", connection);
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+
+                // ComboBox'a değerleri ekle
+                if (dt.Rows.Count > 0)
+                {
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        comboBox1.Items.Add(dt.Rows[i]["DURUM"]);
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void contextMenuStrip1_Click(object sender, EventArgs e)
+        {
+            Onayla();
+        }
+
+        private void dataGridView1_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right)
+            {
+                dataGridView1.Rows[e.RowIndex].Selected = true;
+                rowIndex = e.RowIndex;
+                dataGridView1.CurrentCell = dataGridView1.Rows[rowIndex].Cells[1];
+                this.contextMenuStrip1.Show(dataGridView1, e.Location);
+                contextMenuStrip1.Show(Cursor.Position);
+            }
+        }
+
+        public void Onayla()
+        {
+            string value = dataGridView1.CurrentRow.Cells[10].Value.ToString();
+            MessageBox.Show(value);
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                int selectedRowIndex = dataGridView1.SelectedCells[0].RowIndex;
+                string sip = dataGridView1.Rows[selectedRowIndex].Cells[0].Value.ToString();
+                string yay = dataGridView1.Rows[selectedRowIndex].Cells[1].Value.ToString();
+                string reduk = dataGridView1.Rows[selectedRowIndex].Cells[2].Value.ToString();
+                // Veritabanı bağlantısı
+                if (value == "Onaysız")
+                { 
+                    using (OleDbConnection connection = new OleDbConnection(ConnectionString))
+                    {
+                        connection.Open();
+                        string selectQuery = $"SELECT ONAY FROM Yr_Ustakerim_Proje2_Tab WHERE SIPARIS_NO = '{sip}' AND YAYIN = '{yay}' AND REDUKTOR_KOD = '{reduk}'";
+                        using (OleDbCommand selectCommand = new OleDbCommand(selectQuery, connection))
+                        {
+                            OleDbDataReader reader = selectCommand.ExecuteReader();
+                            if (reader.Read())
+                            {
+                                // Eğer kayıt varsa ONAY durumunu güncelle
+                                string updateQuery = $"UPDATE Yr_Ustakerim_Proje2_Tab SET ONAY = 'Onaylı',ONAY_TARIH = ? WHERE SIPARIS_NO = '{sip}' AND YAYIN = '{yay}' AND REDUKTOR_KOD = '{reduk}'";
+                                using (OleDbCommand updateCommand = new OleDbCommand(updateQuery, connection))
+                                {
+                                    
+                                    updateCommand.Parameters.AddWithValue("ONAY_TARIH", DateTime.Now);
+                                    int rowsAffected = updateCommand.ExecuteNonQuery();
+
+                                    if (rowsAffected > 0)
+                                    {
+                                        dataGridView1.Rows[selectedRowIndex].Cells["ONAY"].Value = "Onaylı";
+                                        MessageBox.Show("Onay durumu güncellendi.");
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("Onay durumu güncellenirken hata oluştu.");
+                                    }
+                                }
+                           
+                            }
+                            else
+                            {
+                                // Eğer kayıt yoksa yeni kayıt ekle
+                                string insertQuery = $"INSERT INTO Yr_Ustakerim_Proje2_Tab (SIPARIS_NO, YAYIN, REDUKTOR_KOD, ONAY,ONAY_TARIH) VALUES ('{sip}', '{yay}', '{reduk}', 'Onaylı',?)";
+                                using (OleDbCommand insertCommand = new OleDbCommand(insertQuery, connection))
+                                {
+                                   
+                                    dataGridView1.Rows[selectedRowIndex].Cells["ONAY"].Value = "Onaylı";
+                                    insertCommand.Parameters.AddWithValue("ONAY_TARIH", DateTime.Now);
+                                    int rowsAffected = insertCommand.ExecuteNonQuery();
+
+                                    if (rowsAffected > 0)
+                                    {
+                                        dataGridView1.Rows[selectedRowIndex].Cells["ONAY"].Value = "Onaylı";
+                                        MessageBox.Show("Yeni Onay eklendi.");
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("Yeni Onay eklenirken hata oluştu.");
+                                    }
+                                }
+                             
+                            }
+                        }
+                    }
+                }
+                else if (value == "Onaylı")
+                {
+                    if (dataGridView1.SelectedCells.Count > 0)
+                    {
+                       // int selectedRowIndex = dataGridView1.SelectedCells[0].RowIndex;
+                        string orderNo = dataGridView1.Rows[selectedRowIndex].Cells[0].Value.ToString();
+                        string lineNo = dataGridView1.Rows[selectedRowIndex].Cells[1].Value.ToString();
+                        string catalogNo = dataGridView1.Rows[selectedRowIndex].Cells[2].Value.ToString();
+                        // Veritabanı bağlantısı
+                        
+                        using (OleDbConnection connection = new OleDbConnection(ConnectionString))
+                        {
+                            connection.Open();
+                            string updateQuery = $"UPDATE Yr_Ustakerim_Proje2_Tab SET ONAY = 'Onaysız',ONAY_TARIH = ? WHERE SIPARIS_NO = '{sip}' AND YAYIN = '{yay}' AND REDUKTOR_KOD = '{reduk}'";
+                            using (OleDbCommand updateCommand = new OleDbCommand(updateQuery, connection))
+                            {
+
+                                updateCommand.Parameters.AddWithValue("ONAY_TARIH", DateTime.Now);
+                                int rowsAffected = updateCommand.ExecuteNonQuery();
+
+                                if (rowsAffected > 0)
+                                {
+                                    dataGridView1.Rows[selectedRowIndex].Cells["ONAY"].Value = "Onaysız";
+                                    MessageBox.Show("Onay durumu güncellendi.");
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Onay durumu güncellenirken hata oluştu.");
+                                }
+                            }
+                          
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Lütfen bir satır seçin.");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir satır seçin.");
+            }
+        }
+        
+        //excele çıktı alma metodu
+        public static void Excel_Disa_Aktar(DataGridView dataGridView1)
+        {
+            Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+            Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+            app.Visible = true;
+            worksheet = workbook.Sheets["Sayfa1"];
+            worksheet = workbook.ActiveSheet;
+            worksheet.Name = "Excel Dışa Aktarım";
+            for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+            {
+                worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+            }
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+            Microsoft.Office.Interop.Excel.Range usedRange = worksheet.UsedRange;
+            usedRange.Columns.AutoFit();
+            usedRange.Rows.AutoFit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Excel_Disa_Aktar(dataGridView1);
+        }
+
+
+    }
+}
